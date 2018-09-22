fe25a73eed8a5b0ab12dda3213ae2369328f2565
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-09-22 18:22:10 +0200

    Add default courses, cuisines and units

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create([{title: 'Appetizer'}, {title: 'Breakfast'}, {title: 'Dessert'}, {title: 'Drink'}, {title: 'Main course'}, {title: 'Sauce'}])
+
+Cuisine.create([{title: 'Brazilian'}, {title: 'Cameroonian'}, {title: 'French'}, {title: 'Fusion'}, {title: 'Greek'}, {title: 'Indian'}, {title: 'Italian'}, {title: 'Moroccan'}, {title: 'Spanish'}, {title: 'Syrian'}, {title: 'Thai'}, {title: 'USA'}])
+
+Unit.create([{title: 'teaspoon'}, {title: 'tablespoon'}, {title: 'cup'}, {title: 'clove'}, {title: 'head'}, {title: 'slice'}, {title: 'ml'}, {title: 'cl'}, {title: 'dl'}, {title: 'l'}, {title: 'g'}, {title: 'mg'}, {title: 'kg'}])
