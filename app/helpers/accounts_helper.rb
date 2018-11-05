2adc6f9c90cfc36515122014fc07463ffb97baf9
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-11-05 17:41:11 +0100

    Fix pagination in account recipe list

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 module AccountsHelper
+  def current_page_first?
+    params[:page].nil? || params[:page].to_i <= 1
+  end
+
+  def current_page_last?
+    @recipes.next_page.blank?
+  end
+
+  def next_page
+    (params[:page] || 1).to_i + 1
+  end
+
+  def previous_page
+    return 1 if params[:page] == 1 || params[:page].nil?
+    (params[:page] || 1).to_i - 1
+  end
 end
