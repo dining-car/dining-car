4dd64d70941e8a0ee46b9a75b4a16cd71a3d37b3
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-11-05 19:18:26 +0100

    Add webp to activestorage variable types

---
+++ config/initializers/active_storage_content_types.rb
@@ -0,0 +1,9 @@
+Rails.application.config.active_storage.variable_content_types = %w(
+  image/png
+  image/gif
+  image/jpg
+  image/jpeg
+  image/webp
+  image/vnd.adobe.photoshop
+  image/vnd.microsoft.icon
+)
