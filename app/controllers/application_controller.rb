a1cac603ba5c72c5fc9d3d611a56f629d9d60c58
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-10-28 10:00:01 +0100

    Remove action cable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,9 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_account
 
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
   protected
 
     def current_account
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,10 +45,6 @@ Rails.application.configure do
     :local
   end
 
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
